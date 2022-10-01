db4e55391282d3b4a2907017470f74868ac0b895
Arenas <cdarenas02@gmail.com>
2022-10-01 11:25:19 -0500

    Se creo el proyecto en Spring boot

---
+++ src/main/java/com/JAHotel/WebHotel/JaHotelApplication.java
@@ -0,0 +1,13 @@
+package com.JAHotel.WebHotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaHotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JaHotelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/JAHotel/WebHotel/JaHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JAHotel.WebHotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JaHotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
